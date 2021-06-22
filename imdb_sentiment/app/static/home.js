a4d3ce7d47848b527c93c0d1254a22c4e741a693
Asutosh-panda <pandasutosh@gmail.com>
2021-06-22 23:32:34 +0530

    first commit

---
+++ imdb_sentiment/app/static/home.js
@@ -0,0 +1,4 @@
+console.log("we are on")
+const analzeFunc =()=>{
+    console.log("click")
+}
---
+++ imdb_sentiment/app/views.py
@@ -0,0 +1,15 @@
+
+from django.shortcuts import render,HttpResponse
+from .mlModel.imdb import imdbModel
+
+# Create your views here.
+
+
+def index(request):
+    if request.method == 'POST':
+        print(request.POST.get('submit'))
+        y_pred = imdbModel(str(request.POST.get('submit')))
+        content={"content":y_pred}
+    
+
+    return render(request,'home.html',context=content)
