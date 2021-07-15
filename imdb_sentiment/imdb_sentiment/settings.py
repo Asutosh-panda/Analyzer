647b1cc33c52556099bee2e84b9cbc50bff6e737
Asutosh-panda <pandasutosh@gmail.com>
2021-07-15 13:46:48 +0530

    fixing responsive issue

--- imdb_sentiment/imdb_sentiment/settings.py
+++ imdb_sentiment/imdb_sentiment/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h2cz*dg^zlfekt51@akwz_93&=i$7v+ecc++)lr+nfl%$0moy@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
